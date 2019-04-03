d5ad7fc97672443538c94aab34d810b56a09c1a2
Jordan Leavitt <jordanleavitt1992@gmail.com>
2019-04-03 07:00:15 -0600

    Functionality added: button pressed to change 8 ball answer, shake gesture to change 8 ball answer, and random answer upon app open.

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -9,12 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageAsset: UIImageView!
+    
+    var randomBallIndex = 0
+    
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateBallImage()
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        updateBallImage()
+    }
+    
+    func updateBallImage() {
+        
+        randomBallIndex = Int(arc4random_uniform(5))
+        
+        imageAsset.image = UIImage(named: ballArray[randomBallIndex])
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        updateBallImage()
+        
+    }
 }
 
